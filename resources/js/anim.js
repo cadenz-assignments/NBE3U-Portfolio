5002f149564e7594925ee73573773b7907b593dd
Caden-Z <zzhan9@ocdsb.ca>
2022-06-17 11:40:17 -0400

    Added downarrow anim

--- resources/js/anim.js
+++ resources/js/anim.js
@@ -1,10 +1,6 @@
-import rive from "@rive-app/canvas";
-
 new rive.Rive({
   // Hosted .riv asset, or a local static asset
   src: "./resources/riv/down_arrow.riv",
   canvas: document.getElementById("down-arrow"),
-  autoplay: true,
+  autoplay: true
 });
-
-console.log("hi");
