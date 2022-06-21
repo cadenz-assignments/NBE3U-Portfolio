260eb8e2fd7acf54013237c508392e662c319299
Caden-Z <zzhan9@ocdsb.ca>
2022-06-21 12:26:52 -0400

    Beautification 101

---
+++ resources/js/scroll.js
@@ -0,0 +1,28 @@
+document.querySelector(".wrapper").addEventListener("scroll", () => {
+    console.log("scrollllllllllllllll");
+    let section = document.getElementById("section-two");
+    let modules = section.children;
+
+    for (let i = 1; i < modules.length; i++) {
+        const module = modules[i];
+        
+        const windowHeight = window.innerHeight;
+        const topPos = module.getBoundingClientRect().top;
+        const topPoint = 150;
+
+        for (let j = 0; j < module.children.length; j++) {
+            const child = module.children[j];
+            let className = "active1";
+            
+            if (j % 2 == 0) {
+                className = "active2";
+            }
+
+            if (topPos < windowHeight - topPoint) {
+                child.classList.add(className);
+            } else {
+                child.classList.remove(className);
+            }
+        }
+    }
+});
